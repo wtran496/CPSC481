19c33efedb3096d7a18c6a4545627381dc7c7195
switch620 <switch620@gmail.com>
2017-10-29 14:45:41 -0700

    Refactored a little and attempted a little clean up

--- hw4.cpp
+++ hw4.cpp
@@ -1,60 +1,74 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
 #include "turtlesim/Pose.h"
-#include <sstream>
 #include <turtlesim/Spawn.h>
 #include <turtlesim/Kill.h>
+#include <sstream>
 #include <string>
 #include <sstream>
 
 using namespace::std;
 
 double getDistance(double x1, double y1, double x2, double y2){
-return sqrt(pow((x1-x2),2) + pow((y1-y2),2));
+	return sqrt(pow((x1-x2),2) + pow((y1-y2),2));
 }
+
 int main(int argc, char **argv){
 
-   	ros::init(argc,argv,"hw4");
-  	ros::NodeHandle n;
-	turtlesim::Pose msg;	//  Defines the message obj
-	stringstream tname;
-	ros::Rate loop_rate(2);
-	int turts = 5;
-	ros::Subscriber Tturts[turts];
-	double pair = 0; //to check the furthest pair
-	turtlesim::Pose msg1;
-	stringstream jname; 
-	double distance = 0; //check pair distances
-	int turtle1 = 1; //store Tturtle1
-	int turtle2 = 1; //store Tturtle2
+	ros::init(argc,argv,"hw4");
 
-	for(int i = 0; i < turts; i ++ ){
+	int num_turts = 3;
+	int turtle1 = 1; // Store Tturtle1
+	int turtle2 = 1; // Store Tturtle2
+	double pair = 0; // To check the furthest pair
+	double distance = 0; // To check pair distances
+	stringstream jname; 
+	stringstream tname;
+
+	ros::NodeHandle n;
+	ros::Rate loop_rate(2);
+	ros::Duration wait_duration = ros::Duration(2);
+
+	//  Defines the message objects
+	turtlesim::Pose msg;	
+	turtlesim::Pose msg1;
+
+	for(int src_turt = 0; src_turt  < num_turts; src_turt ++ ){
 		tname.clear();
 		tname.str("");
-		tname << "/T" << i+1 << "/pose";
+		tname << "/T" << src_turt+1 << "/pose";
+		cout<<"Outer loop"<<tname.str()<<endl;	// Added this for personal visualization purposes-- Delete if you want
 		string name = tname.str();
-		try{msg = *(ros::topic::waitForMessage<turtlesim::Pose>(name, ros::Duration(2)));}
-		catch (int e){ROS_INFO("COULDNT FIND THAT TURT");}
+
+		// Get info about src_turt
+		try {msg = *(ros::topic::waitForMessage<turtlesim::Pose>(name, ros::Duration(2)));}
+		catch (int e) {ROS_INFO("COULDNT FIND THAT TURT");}
 		ROS_INFO("X: %f,  Y: %f", msg.x, msg.y);
-		//i+1 to compare each turtle
-		for (int j = i+1; j < turts; j++){
+
+		// src_turt+1 to compare each turtle
+		for (int dest_turt = src_turt+1; dest_turt < num_turts; dest_turt++){
 			jname.clear();
 			jname.str("");
-			jname << "/T" << j+1 << "/pose";
+			jname << "/T" << dest_turt+1 << "/pose";
+			cout<<"Inner loop"<<jname.str()<<endl; // Added this for personal visualization purposes-- Delete if you want
 			string name = jname.str();
 
+			// Get info about dest_turt
 			try{msg1 = *(ros::topic::waitForMessage<turtlesim::Pose>(name, ros::Duration(2)));}
 			catch (int e){ROS_INFO("COULDNT FIND THAT TURT");}
 			ROS_INFO("X1: %f,  Y1: %f", msg1.x, msg1.y);
-			//get the distance
+
+			// Get the distance
 			double distance = getDistance(msg.x,msg.y,msg1.x,msg1.y);
-			//check the distance
+
+			// Check the distance
 			ROS_INFO("Distance: %f",distance);
+			// Set if this distance is greater
 			if (distance > pair){
 				//Check the pair
 				ROS_INFO("PAIR AND DISTANCE: %f , %f",pair,distance);
-				turtle1 = i+1;
-				turtle2 = j+1;
+				turtle1 = src_turt+1;
+				turtle2 = dest_turt+1;
 				pair = distance;
 			}
 		}
@@ -65,17 +79,18 @@ int main(int argc, char **argv){
 	tname.str("");
 	tname << "/T" << turtle1 << "/pose";
 	string name = tname.str();
-	try{msg = *(ros::topic::waitForMessage<turtlesim::Pose>(name, ros::Duration(2)));}
-	catch (int e){ROS_INFO("COULDNT FIND THAT TURT");}
+	try {msg = *(ros::topic::waitForMessage<turtlesim::Pose>(name, ros::Duration(2)));}
+	catch (int e) {ROS_INFO("COULDNT FIND THAT TURT");}
 	name.clear();
 		
+	jname.clear();
 	jname.str("");
 	jname << "/T" << turtle2 << "/pose";		
 	name = jname.str();
-	try{msg1 = *(ros::topic::waitForMessage<turtlesim::Pose>(name, ros::Duration(2)));}
-	catch (int e){ROS_INFO("COULDNT FIND THAT TURT");}
+	try {msg1 = *(ros::topic::waitForMessage<turtlesim::Pose>(name, ros::Duration(2)));}
+	catch (int e) {ROS_INFO("COULDNT FIND THAT TURT");}
 	name.clear();
 
-	cout << "The furthest pairs are " << turtle1 << " at " << msg.x << "," << msg.y << " and " << turtle2 << " at " << msg1.x << "," << msg1.y;
+	cout << "The furthest pairs are " << turtle1 << " at " << msg.x << "," << msg.y << " and " << turtle2 << " at " << msg1.x << "," << msg1.y<<endl;
    return 0;
 }
