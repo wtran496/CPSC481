633272cde1cb1dba94030598409e24301dfd1c76
switch620 <switch620@gmail.com>
2017-11-01 18:42:52 -0700

    Fixed the turtle checker, now gets the count, and creates arrays to hold them

--- Jhw4.cpp
+++ Jhw4.cpp
@@ -6,9 +6,11 @@
 #include <sstream>
 #include <string>
 #include <sstream>
+#include <boost/algorithm/string.hpp>
 
 const int MAX_TTURTLES = 7;
 const int MAX_XTURTLES = 10;
+turtlesim::Pose turtle1_s;
 
 using namespace::std;
 
@@ -16,70 +18,114 @@ double getDistance(double x1, double y1, double x2, double y2){
 	return sqrt(pow((x1-x2),2) + pow((y1-y2),2));
 }
 
+string getTurtlename(const string topicname) {
+  vector<string> elems;
+  char lc_delim[2];
+  lc_delim[0] = '/';
+  lc_delim[1] = '\0';
+
+  boost::algorithm::split(elems, topicname, boost::algorithm::is_any_of(lc_delim));
+  return elems[1];
+}
+
+bool turtleExist(const string turtlename) {
+  int i;
+  string tname;
+  ros::master::V_TopicInfo alltopics;
+
+  //get all topic names 
+  ros::master::getTopics(alltopics);
+
+  for (int i=0; i<alltopics.size(); i++) {
+     tname = getTurtlename(alltopics[i].name);
+     if (tname.compare(turtlename) == 0) {
+        return true;
+     };
+  };
+  return false;
+}
+
+
+void navigate(turtlesim::Pose t_turts[], ros::NodeHandle n){
+
+	ros::Publisher velocity_publisher = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel",1000);
+
+	double distance_t1, distance_t2;
+	geometry_msgs::Twist vel_msg;
+	turtlesim::Pose t1, t2;
+	t1 = t_turts[0];
+	t2 = t_turts[1];
+
+	distance_t1 = getDistance(turtle1_s.x, turtle1_s.y, t1.x, t1.y);
+	distance_t2 = getDistance(turtle1_s.x, turtle1_s.y, t2.x, t2.y);
+	cout<< "D1: "<< distance_t1 <<"\tD2: "<< distance_t2 << endl;
+	ros::Rate loop_rate(100);
+
+	// Was just testing sending things --probably change this to no longer be a for loop
+	for(int i = 0; i < 100; i++){
+
+		vel_msg.linear.x = 1.5 * distance_t1;
+		vel_msg.linear.y = 0;
+		vel_msg.linear.z = 0;
+
+		vel_msg.angular.x = 0;
+		vel_msg.angular.y = 0;
+		vel_msg.angular.z = 1.1*(atan2(t1.y - turtle1_s.y, t1.x - turtle1_s.x)
+			- turtle1_s.theta); // Kw(θ * - θ)
+
+		velocity_publisher.publish(vel_msg);
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
+}
+
+void poseCallback(const turtlesim::Pose::ConstPtr &pose_message){
+	turtle1_s.x = pose_message -> x;
+	turtle1_s.y = pose_message -> y;
+	turtle1_s.theta = pose_message -> theta;
+}
+
 int main(int argc, char **argv){
-
-	ros::init(argc,argv,"hw4");
-
 	int turtle1 = 1; // Store Tturtle1
 	int turtle2 = 1; // Store Tturtle2
+	int tCount = 0;
+	int xCount = 0;
 	double max_distance = 0; // To check the furthest pair
 	double distance = 0; // To check pair distances
-	stringstream jname; 
-	stringstream tname;
 
+	ros::init(argc,argv,"hw4");
 	ros::NodeHandle n;
+	ros::Subscriber pose_subscriber = n.subscribe("/turtle1/pose",10,poseCallback);
 	ros::Rate loop_rate(2);
 
-	//	Wait-for-message time-out duration
-	ros::Duration wait_duration = ros::Duration(2);
-
-	//  Defines the message objects
-	turtlesim::Pose msg;	
-	turtlesim::Pose msg1;
 	turtlesim::Pose turtle_1;
-	turtlesim::Pose x_turts[MAX_XTURTLES];
-	turtlesim::Pose t_turts[MAX_TTURTLES];
-
-
 	/*
 		COLLECTING ALL TURTLE LOCATIONS
 	*/
-	// Get Turtle1 Information
-	string name = "/turtle1/pose";
-	try { turtle_1= *(ros::topic::waitForMessage<turtlesim::Pose>(name, wait_duration));}
-	catch (int e) { ROS_INFO("COULDNT FIND THAT TURT"); }
-	cout<<"Turtle1: "<<turtle_1.x<<", "<<turtle_1.y<<endl;
-
-	//	Collect all the locations of the X-Turtles in x_turtle[]
-	for(int x_turt = 0; x_turt < MAX_XTURTLES; x_turt++){
-		tname.clear();
-		tname.str("");
-		tname << "/X" << x_turt+1 << "/pose";
-		name = tname.str();
-
-		try { x_turts[x_turt]= *(ros::topic::waitForMessage<turtlesim::Pose>(name, wait_duration));}
-		catch (int e) { ROS_INFO("COULDNT FIND THAT TURT"); }
-	}
-	//	Print all the locations of the X-Turtles to the screen
-	for(int i = 0; i<MAX_XTURTLES; i++){
-		cout<<"X"<<i+1<<": "<<x_turts[i].x<<", "<<x_turts[i].y<<endl;
+	for( int i = 0; i < MAX_TTURTLES; i++){
+		stringstream name;
+		name << "T" << i;
+		cout<<"Checking "<<name.str()<<endl;
+		bool result = turtleExist(name.str());
+		if (result){
+			cout<<name.str()<<" exists!"<<endl;
+			tCount++;
+		}
 	}
 
-	//	Collect all the locations of the T-Turtles in t_turtle[]
-	for(int t_turt = 0; t_turt < MAX_TTURTLES; t_turt++){
-		tname.clear();
-		tname.str("");
-		tname << "/T" << t_turt+1 << "/pose";
-		name = tname.str();
-
-		try { t_turts[t_turt]= *(ros::topic::waitForMessage<turtlesim::Pose>(name, wait_duration));}
-		catch (int e) { ROS_INFO("COULDNT FIND THAT TURT"); }
-	}
-	//	Print all the locations of the T-Turtles to the screen
-	for(int i = 0; i<MAX_TTURTLES; i++){
-		cout<<"T"<<i+1<<": "<<t_turts[i].x<<", "<<t_turts[i].y<<endl;
+	for( int i = 0; i < MAX_TTURTLES; i++){
+		stringstream name;
+		name << "X" << i;
+		cout<<"Checking "<<name.str()<<endl;
+		bool result = turtleExist(name.str());
+		if (result){
+			cout<<name.str()<<" exists!"<<endl;
+			xCount++;
+		}
 	}
 
+	turtlesim::Pose x_turts[xCount];
+	turtlesim::Pose t_turts[tCount];
 
 
 	/*
@@ -117,5 +163,10 @@ int main(int argc, char **argv){
 	<< t_turts[turtle1].x << "," << t_turts[turtle1].y 
 	<< " and " 
 	<< turtle2+1 << " at " << t_turts[turtle2].x << "," << t_turts[turtle2].y<<endl;
+
+	turtlesim::Pose goal_turts[2];
+	goal_turts[0] = t_turts[turtle1];
+	goal_turts[1] = t_turts[turtle2];
    return 0;
 }
+
